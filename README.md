dc9c1ffc6c834192c13a68094dcc15590fba13d8
monotone-RK <kobaya4121@gmail.com>
2014-05-10 22:56:56 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 currency_converter
 ==================
+Currency Converter Written in Python v1.0 last upated:2014.05.10
+
+###Usage
+    usage: curconv.py [-h] [-v] currency_code_from currency_code_to value
+
+    Example: python curconv.py JPY USD 30,000
+
+    positional arguments:
+    currency_code_from  currency code(src)
+    currency_code_to    currency code(dst)
+	value               value you want to convert from currency code(src) you selected
+
+    optional arguments:
+    -h, --help          show this help message and exit
+    -v, --version       show program's version number and exit
---
+++ curconv.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import json
+import urllib2
+import argparse
+import locale
+
+#**  Currency code(ISO 4217) and Locale ID                                   **/
+#******************************************************************************/
+dic = {
+    "JPY": "ja_JP",
+    "USD": "en_US",
+    "EUR": u"\u20AC",  # EURO SIGN
+    "KRW": "ko_KR",
+    "GBP": "en_GB",
+    "CNY": "zh_CN"}
+
+#**  Functions                                                               **/
+#******************************************************************************/
+def currencyConverter(FROM, TO, VALUE):
+    # ----- Error Check ----- #
+    if FROM == "ALL":
+        err_message = 'ERROR!\n"ALL" is not available as curcode_from.\nIf curcode_to, "ALL" can be used.'
+        sys.exit(err_message)
+    elif FROM == TO:
+        err_message = "ERROR!\ncurcode_from and curcode_to are same."
+        sys.exit(err_message)
+    elif not VALUE.replace(",", "").isdigit():
+        err_message = "ERROR!\n%s is invalid value." % VALUE
+        sys.exit(err_message)
+    else:
+        for curcode in [FROM, TO]:
+            if (curcode not in dic) and (TO != "ALL"):
+                err_message = "ERROR!\nThis currency code(%s) is not supported." % curcode
+                sys.exit(err_message)
+
+    VALUE = VALUE.replace(",", "")
+    locale.setlocale(locale.LC_MONETARY, dic[FROM])
+    print "Currency I Have: %s %s" % (locale.currency(int(VALUE), True, True), FROM)
+
+    if TO == "ALL":
+        for key in dic.keys():
+            if not FROM == key:
+                showAmmount(FROM, key, VALUE, dic[key])
+    else:
+        showAmmount(FROM, TO, VALUE, dic[TO])
+
+
+def showAmmount(From, To, Value, LCID):
+    url = "http://rate-exchange.appspot.com/currency?from=%s&to=%s" % (From, To)
+    f = urllib2.urlopen(url).read()
+    jsondata = json.loads(f)
+    howmuch = jsondata["rate"] * int(Value)
+    print "-" * 40
+    if To == "EUR":
+        print To + ":", LCID + str(howmuch), "[Rate: 1 %s -> %s %s]" % (From, str(jsondata["rate"]), To)
+    else:
+        locale.setlocale(locale.LC_MONETARY, LCID)
+        print To + ":", locale.currency(howmuch, True, True), "[Rate: 1 %s -> %s %s]" % (From, str(jsondata["rate"]), To)
+
+#**  Arguments definition                                                    **/
+#******************************************************************************/
+argparser = argparse.ArgumentParser(description="Example: python curconv.py JPY USD 30,000")
+argparser.add_argument("-v", "--version", action="version",
+                       version="Currency Converter Written in Python v1.0 last upated:2014.05.10")
+argparser.add_argument("curcode_from", metavar="currency_code_from",
+                       help="currency code(src)")
+argparser.add_argument("curcode_to", metavar="currency_code_to",
+                       help="currency code(dst)")
+argparser.add_argument("value", metavar="value",
+                       help="value you want to convert from currency code(src) you selected")
+
+#**  main                                                                    **/
+#******************************************************************************/
+args = argparser.parse_args()
+if __name__ == "__main__":
+    currencyConverter(args.curcode_from.upper(),
+                      args.curcode_to.upper(),
+                      args.value)
